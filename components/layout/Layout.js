6286cea676cf49f0710b5eccdee86c572f3bebd6
Jaime Duque <jaime.duque@estratek.com.co>
2020-04-12 17:31:39 -0500

    Css global is added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import Header from './Header'
+import {Global, css} from '@emotion/core'
 
 const Layout = props => {
     return (
         <>
+        <Global 
+            styles={css`
+                :root {
+                    --gray: #3d3d3d;
+                    --gray2: #6F6F6F;
+                    --orange: #DA552F;
+                }
+
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box;
+                }
+
+                *, *:before, *: after {
+                    box-sizing: inherit;
+                }
+                
+                body {
+                    font-size: 1.6rem;
+                    line-height: 1.5;
+                }
+
+                h1, h2, h3 {
+                    margin: 0 0 2rem 0;
+                    line-height: 1.5;
+                }
+
+                ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                a {
+                    text-decoration: none;
+                }
+            `}    
+        />
         <Header />
             <main>
                 {props.children}
