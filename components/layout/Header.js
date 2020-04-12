7068d09e5aa6fadbab2423cbc5ce18c1a4414a0e
Jaime Duque <jaime.duque@estratek.com.co>
2020-04-12 17:12:33 -0500

    Navigation is created

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Search from '../ui/Search'
+import Navigation from './Navigation'
+import Link from 'next/link'
 
 const Header = () => {
     return ( 
@@ -8,10 +10,13 @@ const Header = () => {
                 <div>
                     <p>P</p>
                     <Search />
-                    {/* Nav here */}
+                    <Navigation />
                 </div>
                 <div>
-                    {/* Administration menu */}
+                    <p>Hello: Jaime</p>
+                    <button type="button">Log Out</button>
+                    <Link href="/">Log In</Link>
+                    <Link href="/">Sign Up</Link>
                 </div>
             </div>
         </header>
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link'
 import Header from './Header'
 
 const Layout = props => {
