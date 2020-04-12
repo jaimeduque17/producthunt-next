f1098b78699eae7574fa55fd336564300d46cdbf
Jaime Duque <jaime.duque@estratek.com.co>
2020-04-12 16:57:25 -0500

    Header and Search component are created

---
+++ components/ui/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = () => {
+    return ( 
+        <form>
+            <input type="text" />
+            <button type="submit">
+                Search
+            </button>
+        </form>
+     );
+}
+ 
+export default Search;
---
+++ pages/aboutUs.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/Layout'
+
+const AboutUs = () => (
+  <div>
+    <Layout>
+      <h1>AboutUs</h1>
+    </Layout>
+  </div>
+)
+
+export default AboutUs
