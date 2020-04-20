ea4ddde2a579a883d925279db2fc11cf6927f049
Jaime Duque <jaime.duque@estratek.com.co>
2020-04-19 23:16:41 -0500

    Product component is updated

--- components/layout/ProductDetails.js
+++ components/layout/ProductDetails.js
@@ -1,15 +1,113 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+// if i want to change the language
+// import {es} from 'date-fns/locale' 
+// and then {formatDistanceToNow(new Date(created), {locale: es})}
+
+const Product = styled.li`
+    padding: 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #e1e1e1;
+`
+
+const ProductDescription = styled.div`
+    flex: 0 1 600px;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    column-gap: 2rem;
+`
+
+const Description = styled.p`
+    font-size: 1.6rem;
+    margin: 0;
+    color: #888;
+`
+
+const Title = styled.a`
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 0;
+
+    :hover {
+        cursor: pointer;
+    }
+`
+
+const Comments = styled.div`
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+
+    div {
+        display: flex;
+        align-items: center;
+        border: 1px solid #e1e1e1;
+        padding: .3rem 1rem;
+        margin-right: 2rem;
+    }
+    img {
+        width: 2rem;
+        margin-right: 2rem;
+    }
+    p {
+        font-size: 1.6rem;
+        margin-right: 1rem;
+        font-weight: 700;
+        &:last-of-type {
+            margin: 0;
+        }
+    }
+`
+
+const Image = styled.img`
+    width: 200px;
+`
+
+const Votes = styled.div`
+    flex: 0 0 auto;
+    text-align: center;
+    border: 1px solid #e1e1e1;
+    padding: 1rem 3rem;
+
+    div {
+        font-size: 2rem;
+    }
+
+    p {
+        font-size: 2rem;
+        margin: 0;
+        font-weight: 700;   
+    }
+`
 
 const ProductDetails = ({ product }) => {
     const { id, comments, created, description, company, name, url, imageUrl, vote } = product
     return (
-        <li>
-            <div>
-                <div></div>
-                <div>{name}</div>
-            </div>
-            <div></div>
-        </li>
+        <Product>
+            <ProductDescription>
+                <div>
+                    <Image src={imageUrl} />
+                </div>
+                <div>
+                    <Title>{name}</Title>
+                    <Description>{description}</Description>
+                    <Comments>
+                        <div>
+                            <img src="/static/img/comment.png" />
+                            <p>{comments.length} Comments</p>
+                        </div>
+                    </Comments>
+                    <p>Published {formatDistanceToNow(new Date(created))} ago</p>
+                </div>
+            </ProductDescription>
+            <Votes>
+                <div>&#9650;</div>
+                <p>{vote}</p>
+            </Votes>
+        </Product>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,49 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { FirebaseContext } from '../firebase'
 import Layout from '../components/layout/Layout'
+import ProductDetails from '../components/layout/ProductDetails'
 
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
-  </div>
-)
+const Home = () => {
+
+  const [products, setProducts] = useState([])
+
+  const { firebase } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handlerSnapshot)
+    }
+    getProducts()
+  }, [])
+
+  function handlerSnapshot(snapshot) {
+    const products = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    setProducts(products)
+  }
+
+  return (
+    <div>
+      <Layout>
+        <div className="products-list">
+          <div className="container">
+            <ul className="bg-white">
+              {products.map(product => (
+                <ProductDetails 
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
 
 export default Home
